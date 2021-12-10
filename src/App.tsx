39685568728c8019711558ec97812c831058c363
waterkitten <y1972352623@163.com>
2021-12-10 16:23:55 +0800

    button组件编写成功，可以有size和btnType便于选择

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Button from './components/Button';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* button的demo演示 */}
+      <Button
+        size='lg'
+        btnType='primary'
+        disabled 
+      >primary</Button>
+        <Button
+        size='sm'
+        btnType='danger'
+      >danger</Button>
+      <Button
+        size='sm'
+        btnType='default'
+      >default</Button>
+      <Button
+        size='lg'
+        btnType='link'
+        href='https://zh-hans.reactjs.org/docs/create-a-new-react-app.html'
+      >default link</Button>
+      <div>22</div>
     </div>
   );
 }
