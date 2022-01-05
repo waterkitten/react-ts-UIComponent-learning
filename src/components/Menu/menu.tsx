37a11e7d578695844346f4c7eac3c19d56346e90
waterkitten <y1972352623@163.com>
2022-01-05 19:18:03 +0800

    feat:添加子菜单的消息提示

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -5,23 +5,23 @@ import { MenuItemProps } from './menuItem';
 type MenuMode = 'horizontal' | 'vertical'
 export interface MenuProps {
   /**默认 active 的菜单项的索引值 */
-  defaultIndex?: number;
+  defaultIndex?: string;
   className?: string;
   /**菜单类型 横向或者纵向 */
   mode?: MenuMode;
   style?: CSSProperties;
   /**点击菜单项触发的回掉函数 */
-  onSelect?: (selectedIndex: number) => void;
+  onSelect?: (selectedIndex: string) => void;
 }
 
 interface IMenuContext{
-    index:number;
-    onSelect?:(selectedIndex:number)=>void;
+    index:string;
+    onSelect?:(selectedIndex:string)=>void;
     mode?:MenuMode;
 }
 //context 
 
-export const MenuContext = createContext<IMenuContext>({ index: 0 })
+export const MenuContext = createContext<IMenuContext>({ index: '0' })
 
 /**?
  * @description menu组件支持横向纵向两种模式
@@ -34,14 +34,14 @@ export const Menu:FC<MenuProps>=(props)=>{
         'menu-horizontal': mode !== 'vertical',
     })
     //处理事件点击时候切换样式
-    const handleSelect = (index: number) => {
+    const handleSelect = (index: string) => {
         setActive(index)
         if (onSelect) {
           onSelect(index)
         }
       }
     const passedContext:IMenuContext={
-        index:currentActive?currentActive:0,
+        index:currentActive?currentActive:'0',
         onSelect:handleSelect,
         mode
     }
@@ -51,7 +51,7 @@ export const Menu:FC<MenuProps>=(props)=>{
             const {displayName}=childElement.type
             if(displayName==="MenuItem" || displayName === 'SubMenu'){
                 return React.cloneElement(childElement,{
-                    index
+                    index:index.toString()
                 })
             }else {
                 console.error("Warning: Menu has a child which is not a MenuItem component")
@@ -68,7 +68,7 @@ export const Menu:FC<MenuProps>=(props)=>{
    )
 }
 Menu.defaultProps = {
-    defaultIndex:0,
+    defaultIndex:'0',
     mode: 'horizontal',
 }
 export default Menu;
